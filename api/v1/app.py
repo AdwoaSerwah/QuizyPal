44d57a234b3aa607d8a880272b0c1e807425062a
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2025-01-06 23:45:13 +0000

    Move all initializations from app.py to config.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,48 +3,46 @@
 Route module for the API
 """
 
-from dotenv import load_dotenv
 from flask import Flask, jsonify
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from os import getenv
-from typing import Optional
-from celery import Celery
+from flask_jwt_extended import JWTManager
+from flasgger import Swagger
+from config import Config, mail, make_celery
 from api.v1.views import app_views
 from models import storage
-# from sqlalchemy.exc import OperationalError
-# import time
-
-
-
-# Load environment variables from a .env file
-load_dotenv()
-
-def make_celery(app):
-    celery = Celery(
-        app.import_name,
-        broker=app.config['REDIS_URL'],
-        backend=app.config['REDIS_URL']
-    )
-    celery.conf.update(app.config)
-    return celery
+from os import getenv, environ
+from flask_mail import Message
 
 # Initialize Flask app
 app = Flask(__name__)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+app.config.from_object(Config)
 
-# Configure the app with the database URL
-app.config['SQLALCHEMY_DATABASE_URI'] = getenv("DATABASE_URL")
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Suppress the warning
-app.config['SECRET_KEY'] = getenv('SECRET_KEY')
-app.config['REDIS_URL'] = getenv("REDIS_URL")
+# Initialize JWTManager
+jwt = JWTManager(app)
 
-# Initialize SQLAlchemy object
+# Initialize SQLAlchemy and Flask-Migrate
 db = SQLAlchemy(app)
-
-# Initialize Flask-Migrate with app and Base (Base contains the models' metadata)
 migrate = Migrate(app, db)
+
+# Initialize Flask-Mail
+mail.init_app(app)
+
+
+"""
+with app.app_context():
+    msg = Message('Test Email',
+                  sender='takeabite56@gmail.com',
+                  recipients=['adwoaserwahkyeibaffour@gmail.com',
+                              'lindseylinwood56@gmail.com'])
+    msg.body = 'This is a test email.'
+    try:
+        mail.send(msg)
+        print("Test email sent.")
+    except Exception as e:
+        print(f"Error sending test email: {e}")
+"""
 
 # Create Celery app
 celery = make_celery(app)
@@ -52,50 +50,8 @@ celery = make_celery(app)
 # Register the blueprint for API routes
 app.register_blueprint(app_views)
 
-# Enable CORS for the API, allowing all origins for `/api/v1/*` routes
+# Enable CORS for the API
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-
-# Error Handlers
-@app.errorhandler(404)
-def not_found(error) -> str:
-    """
-    Handles 404 Not Found errors.
-
-    Args:
-        error: The error object.
-
-    Returns:
-        A JSON response with the error message and a 404 status code.
-    """
-    return jsonify({"error": "Not found"}), 404
-
-
-@app.errorhandler(401)
-def unauthorized_error(error) -> str:
-    """
-    Handles 401 Unauthorized errors.
-
-    Args:
-        error: The error object.
-
-    Returns:
-        A JSON response with the error message and a 401 status code.
-    """
-    return jsonify({"error": "Unauthorized"}), 401
-
-
-@app.errorhandler(403)
-def forbidden_error(error) -> str:
-    """
-    Handles 403 Forbidden errors.
-
-    Args:
-        error: The error object.
-
-    Returns:
-        A JSON response with the error message and a 403 status code.
-    """
-    return jsonify({"error": "Forbidden"}), 403
 
 
 # Utility function to provide application context for database testing
@@ -109,16 +65,75 @@ def create_app() -> Flask:
     Returns:
         Flask: The created Flask application instance.
     """
+    from models.engine.db_storage import DBStorage
+
+    # Set the environment to 'test' to use the test database
+    environ["FLASK_ENV"] = "test"
+
+    # Create a new instance of DBStorage for testing
+    storage = DBStorage()
+
     with app.app_context():
         storage.reload()  # Ensure storage is fully initialized
     return app
 
 
+@app.teardown_appcontext
+def close_db(error):
+    """ Close Storage """
+    storage.close()
+
+
+# Error Handlers
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """
+    Handles 404 Not Found errors.
+
+    Args:
+        error: The error object.
+
+    Returns:
+        A JSON response with the error message and a 404 status code.
+    """
+    message = getattr(error, "description", "Not found")
+    return jsonify({'error': message}), 404
+
+
+@app.errorhandler(401)
+def unauthorized_error(error) -> str:
+    """
+    Handles 401 Unauthorized errors.
+
+    Args:
+        error: The error object.
+
+    Returns:
+        A JSON response with the error message and a 401 status code.
+    """
+    message = getattr(error, "description", "Unauthorized")
+    return jsonify({"error": message}), 401
+
+
+@app.errorhandler(403)
+def forbidden_error(error) -> str:
+    """
+    Handles 403 Forbidden errors.
+
+    Args:
+        error: The error object.
+
+    Returns:
+        A JSON response with the error message and a 403 status code.
+    """
+    message = getattr(error, "description", "Forbidden")
+    return jsonify({"error": message}), 403
+
+# Swagger setup
+app.config['SWAGGER'] = {'title': 'QuizyPal Restful API', 'uiversion': 1}
+Swagger(app)
+
 if __name__ == "__main__":
-    # Read host and port from environment variables,
-    # default to "0.0.0.0" and "5000"
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-
-    # Start the Flask application in debug mode
-    app.run(host=host, port=port, debug=True)
+    app.run(host=host, port=port, threaded=True, debug=True)
